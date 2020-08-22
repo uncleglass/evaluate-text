4b5260a1f4e4b5b718616b46d2504a8c97521a5f
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-08-22 04:54:00 +0200

    feat: Add main view

--- src/server/index.js
+++ src/server/index.js
@@ -1,29 +1,34 @@
 // Express to run server and routes
-const express = require('express');
+const express = require("express");
 
 // Start up an instance of app
 const app = express();
 
 /* Dependencies */
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
+const path = require("path");
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Cors for cross origin allowance
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
+app.use(express.static("src/client"));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.resolve("src/client/views/index.html"));
+});
 
 const port = 8080;
 app.listen(port, listening);
 
-function listening () {
-    console.log('#################################');
-    console.log('##                             ##');
-    console.log('##      Server running!!!      ##');
-    console.log(`## App listening on port ${port}: ##`);
-    console.log('##                             ##');
-    console.log('#################################');
-
-};
+function listening() {
+  console.log("#################################");
+  console.log("##                             ##");
+  console.log("##      Server running!!!      ##");
+  console.log(`## App listening on port ${port}: ##`);
+  console.log("##                             ##");
+  console.log("#################################");
+}
