63b1bd3641f117c7e2e1fa641fc167dc52320913
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-08-26 23:29:03 +0200

    feat: Add service worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
@@ -46,5 +47,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].css",
     }),
+    new WorkboxPlugin.GenerateSW(),
   ],
 };
